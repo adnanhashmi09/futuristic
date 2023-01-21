c1e8bd945169b63668240548678f23a1977109f8
adnanhashmi09 <hashmi.adnan791@gmail.com>
2023-01-21 20:55:03 +0530

    inter font added and cta completed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,20 @@
 import styles from "@/styles/components/footer.module.scss";
+import Link from "next/link";
 
 function Footer(): JSX.Element {
   return (
     <footer className={styles.footer}>
       <h2>Futuristic </h2>
       <ul>
-        <li> Work </li>
-        <li> Our Process </li>
-        <li> Contact </li>
+        <li>
+          <Link href=""> Work</Link>
+        </li>
+        <li>
+          <Link href="">Our Process</Link>
+        </li>
+        <li>
+          <Link href=""> Contact </Link>
+        </li>
       </ul>
     </footer>
   );
